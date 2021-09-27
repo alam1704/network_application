fafc33a15bf4aadfe575f4549d7cf656370e7132
alexlam <alexlam1994@live.com>
2021-09-27 19:50:02 +1000

    starting a new game file to try again

--- single_player.py
+++ single_player.py
@@ -2,27 +2,6 @@ import os
 import time
 from grid import Board
 
-import threading
-# create a separate thread to send and receive data from the server
-def create_thread(target):
-    thread = threading.Thread(target=target)
-    thread.daemon = True #thread that runs in the background to support main/non-daemon thread
-    thread.start() #this will start a separate thread
-
-import socket
-
-HOST = '127.0.0.1'
-PORT = 65432
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
-
-def receive_data():
-    #receive data from server
-    pass
-
-create_thread(receive_data)
-
 board=Board()
 
 def clear():
